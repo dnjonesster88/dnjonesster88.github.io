4ba1da6aacb85389f9025715c07efb0796627eef
Diane Jones <dnjonesster@gmail.com>
2026-04-18 13:19:09 -0700

    Add async function to build and display cards

--- js/cards.js
+++ js/cards.js
@@ -1,1 +1,19 @@
+async function buildCards() {
+  const response = await fetch('/data/pages.json');
+  const { pages } = await response.json();
 
+  const grid = document.getElementById('card-grid');
+
+  pages.forEach(page => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+      <div class="card-icon">${page.icon}</div>
+      <h3><a href="${page.url}">${page.title}</a></h3>
+      <p>${page.description}</p>
+    `;
+    grid.appendChild(card);
+  });
+}
+
+buildCards();
